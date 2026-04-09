d1966fe28210c270334c70f40428d0cd869132de
JEODI <156963828+OdiaseJessica@users.noreply.github.com>
2026-04-09 19:58:07 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import cv2
-import pytesseract
-import os
+#import cv2
+#import pytesseract
+#import os
 
-image = cv2.imread("")
-text = pytesseract.image_to_string(image)
-print(text)
+#image = cv2.imread("")
+#text = pytesseract.image_to_string(image)
+#print(text)
+#def extract_text_from_image(image_path):
+    #image = cv2.imread(image_path)
+    #text = pytesseract.image_to_string(image)
+   # return text
